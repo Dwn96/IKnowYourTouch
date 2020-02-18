9845b9f7b91a187ebc97c62f10c95a701e986f41
Dwn96 <45044744+Dwn96@users.noreply.github.com>
2020-02-18 18:10:51 +0300

    Initial commit

---
+++ app/src/main/java/com/wambu/iknowyourtouch/MainActivity.java
@@ -0,0 +1,125 @@
+package com.wambu.iknowyourtouch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.text.Layout;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener, View.OnTouchListener {
+
+    private Button bigRedButton;
+    private GestureDetector mGestureDetector;
+    Context context= this;
+    MediaPlayer mp;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     bigRedButton=findViewById(R.id.push_btn);
+
+
+        bigRedButton.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(this,this);
+
+
+
+
+
+
+
+
+
+    }
+
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        mp = MediaPlayer.create(context, R.raw.doubletap);
+        Toast.makeText(this,"You've double tapped me",Toast.LENGTH_SHORT).show();
+        try {
+            if (mp.isPlaying()) {
+                mp.stop();
+                mp.release();
+
+            } mp.start();
+        } catch(Exception t) { t.printStackTrace(); }
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        mp= MediaPlayer.create(context,R.raw.longpress);
+        Toast.makeText(this,"You've long pressed me",Toast.LENGTH_SHORT).show();
+        try{
+            if (mp.isPlaying()){
+                mp.stop();
+                mp.release();
+
+            }mp.start();
+        }
+        catch (Exception t){
+            t.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        if(view.getId() == R.id.push_btn){
+            //methods for touching button
+            mGestureDetector.onTouchEvent(event);
+            return true;
+        }
+
+        return true;
+    }
+}
+
